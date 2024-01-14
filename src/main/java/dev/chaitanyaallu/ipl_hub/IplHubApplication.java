607570c72af946d502233a2cf60f819284c3ac70
Chaitanya Swaroop Kumar Allu <chaitanyaa1999@gmail.com>
2024-01-14 14:44:19 -0600

    added team api endpoint

---
+++ src/main/java/dev/chaitanyaallu/ipl_hub/IplHubApplication.java
@@ -0,0 +1,13 @@
+package dev.chaitanyaallu.ipl_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplHubApplication.class, args);
+	}
+
+}
